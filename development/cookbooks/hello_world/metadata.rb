8b1da59708747e60e3437d22492fdeb03636b888
adin <adin@ip-10-252-15-136.us-west-2.compute.internal>
2013-01-28 18:12:27 +0000

    Added a hello world message to the default.rb recipe

--- development/cookbooks/hello_world/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Station X"
-maintainer_email "adin@stationxinc.com"
-license          "All rights reserved"
-description      "Installs/Configures hello_world"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
-
-recipe "hello_world::default", "Prints hello world to the audit entry"
