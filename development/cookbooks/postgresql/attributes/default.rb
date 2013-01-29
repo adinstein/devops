26910c56906e0a499a6ec990c7fa79c571d2f46c
adin <adin@ip-10-252-15-136.us-west-2.compute.internal>
2013-01-29 19:56:41 +0000

    changed defualt version of psotgres to 9.1

--- development/cookbooks/postgresql/attributes/default.rb
+++ development/cookbooks/postgresql/attributes/default.rb
@@ -21,9 +21,9 @@ case platform
 when "debian"
 
   if platform_version.to_f == 5.0
-    default[:postgresql][:version] = "9.2"
+    default[:postgresql][:version] = "9.1"
   elsif platform_version =~ /squeeze/
-    default[:postgresql][:version] = "9.2"
+    default[:postgresql][:version] = "9.1"
   end
 
   set[:postgresql][:dir] = "/etc/postgresql/#{node[:postgresql][:version]}/main"
@@ -32,11 +32,11 @@ when "ubuntu"
 
   case
   when platform_version.to_f <= 9.04
-    default[:postgresql][:version] = "9.2"
+    default[:postgresql][:version] = "9.1"
   when platform_version.to_f <= 11.04
-    default[:postgresql][:version] = "9.2"
+    default[:postgresql][:version] = "9.1"
   else
-    default[:postgresql][:version] = "9.2"
+    default[:postgresql][:version] = "9.1"
   end
 
   set[:postgresql][:dir] = "/etc/postgresql/#{node[:postgresql][:version]}/main"
@@ -44,29 +44,29 @@ when "ubuntu"
 when "fedora"
 
   if platform_version.to_f <= 12
-    default[:postgresql][:version] = "9.2"
+    default[:postgresql][:version] = "9.1"
   else
-    default[:postgresql][:version] = "9.2"
+    default[:postgresql][:version] = "9.1"
   end
 
   set[:postgresql][:dir] = "/var/lib/pgsql/data"
 
 when "redhat","centos","scientific","amazon"
 
-  default[:postgresql][:version] = "9.2"
+  default[:postgresql][:version] = "9.1"
   set[:postgresql][:dir] = "/var/lib/pgsql/data"
 
 when "suse"
 
   if platform_version.to_f <= 11.1
-    default[:postgresql][:version] = "9.2"
+    default[:postgresql][:version] = "9.1"
   else
-    default[:postgresql][:version] = "9.2"
+    default[:postgresql][:version] = "9.1"
   end
 
   set[:postgresql][:dir] = "/var/lib/pgsql/data"
 
 else
-  default[:postgresql][:version] = "9.2"
+  default[:postgresql][:version] = "9.1"
   set[:postgresql][:dir]         = "/etc/postgresql/#{node[:postgresql][:version]}/main"
 end
