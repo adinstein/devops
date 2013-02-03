9edf3d92afd5088c93dbd8e17c0e1b424a46687d
adin <adin@stationxinc.com>
2013-02-02 23:01:48 -0800

    addin yum_package to get around chef bug

--- cookbooks/stx_db_postgres/recipes/stx_pg_install.rb
+++ cookbooks/stx_db_postgres/recipes/stx_pg_install.rb
@@ -7,6 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
+#need to enable PostgreSQL Global Development Group yum repository maintained by Devrim Gündüz for updated PostgreSQL packages
+include_recipe 'postgresql::yum_pgdg_postgresql'
+
 
 yum_package "postgresql-plperl.x86_64" do
   action [:install]
