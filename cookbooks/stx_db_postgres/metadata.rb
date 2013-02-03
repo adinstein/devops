b4fa6204e714d2498a13ff5213ffa8546091f2ee
adin <adin@stationxinc.com>
2013-02-02 20:39:16 -0800

    more cleanup

---
+++ cookbooks/stx_db_postgres/metadata.rb
@@ -0,0 +1,9 @@
+name             'stx_db_postgres'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures stx_db_postgres'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+recipe "stx_db_postgres::stx_pg_install","Installs and Configures base postgres install for Station X"
---
+++ cookbooks/stx_db_postgres/recipes/stx_pg_install.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: stx_db_postgres
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "ipostgresql-plperl.x86_64" do
+  action [:install]
+end
+
