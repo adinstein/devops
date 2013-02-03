959e8f752da1fda225c13c9d2db62272c4ba6b7e
adin <adin@stationxinc.com>
2013-02-02 23:25:11 -0800

    added yum repo recipe and additional postgres packages

--- cookbooks/stx_db_postgres/recipes/stx_pg_install.rb
+++ cookbooks/stx_db_postgres/recipes/stx_pg_install.rb
@@ -1,17 +1,29 @@
 #
 # Cookbook Name:: stx_db_postgres
-# Recipe:: default
+# Recipe:: stx_pg_install.rb
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, cwStation X Inc.
 #
 # All rights reserved - Do Not Redistribute
 #
 
-#need to enable PostgreSQL Global Development Group yum repository maintained by Devrim Gündüz for updated PostgreSQL packages
 include_recipe 'postgresql::yum_pgdg_postgresql'
 
 
-yum_package "postgresql-plperl.x86_64" do
+yum_package "postgresql91-server.x86_64" do
   action [:install]
 end
+
+yum_package "postgresql91-devel.x86_64" do
+  action [:install]
+end
+
+yum_package "postgresql91-contrib.x86_64" do
+  action [:install]
+end
+
+yum_package "postgresql91-plperl.x86_64" do
+  action [:install]
+end
+
 
