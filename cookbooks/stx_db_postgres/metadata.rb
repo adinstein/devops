cc4129c579b3b158b6bf9d6f04d7c059d3f9af41
adin <adin@stationxinc.com>
2013-02-02 23:42:20 -0800

    adding dependancy on postgres cookbook

--- cookbooks/stx_db_postgres/metadata.rb
+++ cookbooks/stx_db_postgres/metadata.rb
@@ -6,4 +6,6 @@ description      'Installs/Configures stx_db_postgres'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "postgresql"
+
 recipe "stx_db_postgres::stx_pg_install","Installs and Configures base postgres install for Station X"
